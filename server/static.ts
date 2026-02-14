a0e6619052caea040a9969a8a182e2072bb926e8
Akshay Misra <akshaycoding123@gmail.com>
2026-02-14 11:49:46 +0530

    Fixed bug

--- server/static.ts
+++ server/static.ts
@@ -27,7 +27,7 @@ export function serveStatic(app: Express) {
   }));
 
   // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
+  app.get("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
